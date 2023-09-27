290a85642dcc3d0ce3389abbfbaf220fb140444d
Fabricio Villalobos <fabricio.villalobos@gmail.com>
2023-09-26 21:57:22 -0600

    Update copy-code-button.js

--- assets/js/copy-code-button.js
+++ assets/js/copy-code-button.js
@@ -1,1 +1,16 @@
+function createCopyButton(highlightDiv) {
+  const button = document.createElement("button");
+  button.className = "copy-code-button";
+  button.type = "button";
+  button.innerText = "Copy";
+  button.addEventListener("click", () => copyCodeToClipboard(button, highlightDiv));
+  highlightDiv.insertBefore(button, highlightDiv.firstChild);
+
+  const wrapper = document.createElement("div");
+  wrapper.className = "highlight-wrapper";
+  highlightDiv.parentNode.insertBefore(wrapper, highlightDiv);
+  wrapper.appendChild(highlightDiv);
+}
+
+document.querySelectorAll(".highlight").forEach((highlightDiv) => createCopyButton(highlightDiv));
 
