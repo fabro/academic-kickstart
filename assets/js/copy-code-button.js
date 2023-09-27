f608ede102f1611d8a69c4cd6e8d925438d0c4ef
Fabricio Villalobos <fabricio.villalobos@gmail.com>
2023-09-26 22:00:18 -0600

    Update copy-code-button.js

--- assets/js/copy-code-button.js
+++ assets/js/copy-code-button.js
@@ -14,3 +14,38 @@ function createCopyButton(highlightDiv) {
 
 document.querySelectorAll(".highlight").forEach((highlightDiv) => createCopyButton(highlightDiv));
 
+async function copyCodeToClipboard(button, highlightDiv) {
+  const codeToCopy = highlightDiv.querySelector(":last-child > .chroma > code").innerText;
+  try {
+    var result = await navigator.permissions.query({ name: "clipboard-write" });
+    if (result.state == "granted" || result.state == "prompt") {
+      await navigator.clipboard.writeText(codeToCopy);
+    } else {
+      copyCodeBlockExecCommand(codeToCopy, highlightDiv);
+    }
+  } catch (_) {
+    copyCodeBlockExecCommand(codeToCopy, highlightDiv);
+  } finally {
+ button.blur();
+  button.innerText = "Copied!";
+  setTimeout(function () {
+    button.innerText = "Copy";
+  }, 2000);  }
+}
+
+function copyCodeBlockExecCommand(codeToCopy, highlightDiv) {
+  const textArea = document.createElement("textArea");
+  textArea.contentEditable = "true";
+  textArea.readOnly = "false";
+  textArea.className = "copyable-text-area";
+  textArea.value = codeToCopy;
+  highlightDiv.insertBefore(textArea, highlightDiv.firstChild);
+  const range = document.createRange();
+  range.selectNodeContents(textArea);
+  const sel = window.getSelection();
+  sel.removeAllRanges();
+  sel.addRange(range);
+  textArea.setSelectionRange(0, 999999);
+  document.execCommand("copy");
+  highlightDiv.removeChild(textArea);
+}
